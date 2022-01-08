c840380a03115d5ce3964dd4e99117d07c2a3a7c
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2022-01-08 20:58:30 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ read -p "Enter Your hostname (Example: vpn.example.com or  123.123.123.123): "  
 echo "Is correct hostname:  $hostname!"
 
 while true; do
-    read -p "Do you wish to install this program?" yn
+    read -p "Do you wish to install this program? " yn
     case $yn in
         [Yy]* ) docker-compose run --rm openvpn ovpn_genconfig -u udp://$hostname  && docker-compose run --rm openvpn ovpn_initpki && exit;;
         [Nn]* ) exit;;
